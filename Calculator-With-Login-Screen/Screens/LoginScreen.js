c015d952a0c8b845d9280dd8f74492a130045979
Sai Prathik R <saiprathik2419@gmail.com>
2024-02-02 14:23:37 +0530

    Add dependencies for react-native-web and react-dom

--- Calculator-With-Login-Screen/Screens/LoginScreen.js
+++ Calculator-With-Login-Screen/Screens/LoginScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
-
 export default function LoginScreen({ navigation }) {
 
     return (
